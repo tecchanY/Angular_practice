55195215d4a0cab1df994c42f71255c3a2ae7fa8
山口 哲平 <teppei.yamaguchi@akashic.home>
2019-11-28 13:09:15 +0900

    サブモジュールを解決

---
+++ angular-tour-of-heroines/src/app/heroines/heroines.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { Heroine } from "../Heroine";
+import { HEROINES } from "../mock-heroines";
+
+@Component({
+  selector: "app-heroines",
+  templateUrl: "./heroines.component.html",
+  styleUrls: ["./heroines.component.scss"]
+})
+export class HeroinesComponent implements OnInit {
+  // heroine = "シータ";
+
+  // heroine: Heroine = {
+  //   id: 1,
+  //   name: "シータ"
+  // };
+  heroines = HEROINES;
+
+  selectedHeroine: Heroine;
+  onSelect(heroine: Heroine): void {
+    this.selectedHeroine = heroine;
+    console.log(heroine); //確認用
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
