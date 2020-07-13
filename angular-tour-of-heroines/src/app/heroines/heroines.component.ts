d843c9d8b82b319e4aabca4040676f8b2a86fdc4
山口 哲平 <teppei.yamaguchi@akashic.co.jp>
2020-07-13 11:56:53 +0900

    リスト表示とOnSelect修正

--- angular-tour-of-heroines/src/app/heroines/heroines.component.ts
+++ angular-tour-of-heroines/src/app/heroines/heroines.component.ts
@@ -5,7 +5,7 @@ import { HEROINES } from "../mock-heroines";
 @Component({
   selector: "app-heroines",
   templateUrl: "./heroines.component.html",
-  styleUrls: ["./heroines.component.scss"]
+  styleUrls: ["./heroines.component.scss"],
 })
 export class HeroinesComponent implements OnInit {
   // heroine = "シータ";
@@ -15,14 +15,13 @@ export class HeroinesComponent implements OnInit {
   //   name: "シータ"
   // };
   heroines = HEROINES;
-
   selectedHeroine: Heroine;
+
+  constructor() {}
+  ngOnInit() {}
+
   onSelect(heroine: Heroine): void {
     this.selectedHeroine = heroine;
     console.log(heroine); //確認用
   }
-
-  constructor() {}
-
-  ngOnInit() {}
 }
